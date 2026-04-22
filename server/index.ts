7c5920b6aebde3817f7ef0dcbec0dbe966470654
davidvato <davidvato@hotmail.com>
2026-04-21 23:13:49 -0600

    Fix Express 5 wildcard route: use named catch-all parameter

--- server/index.ts
+++ server/index.ts
@@ -972,7 +972,7 @@ app.delete('/api/tournaments/:id', async (req, res) => {
 });
 
 // All other GET requests not handled before will return the React app
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
